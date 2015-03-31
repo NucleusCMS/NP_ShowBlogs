2b3150b5a9cc39521f46316f1a150ca22959ab4b
Osamu Higuchi <osamu@higuchi.com>
2015-03-31 16:54:26 +0900

    Revert "Configurable pageswitch format"
    This reverts commit ee6b2a4648c0401be658bc2141e6ff31a35f994c.

--- showblogs/english.php
+++ showblogs/english.php
@@ -11,17 +11,6 @@
 	define('_TAG_MODE',     'TagEX narrow mode on page-switch');
 	define('_SB_NEXTL',     'Link text for NextPage');
 	define('_SB_PREVL',     'Link text for PrevPage');
-	define('_SB_NEXTSEP',   'Separator text before the NextPage link');
-	define('_SB_PREVSEP',   'Separator text after the PrevPage link');
-	define('_SB_PAGESEP2',  'Separator text between the pages for Type 2 page switch');
-	define('_SB_PAGESEP3',  'Separator text between the pages for Type 3 page switch');
-	define('_SB_PGLSTHDR',  'Header text before the page link list');
-	define('_SB_PGLSTFTR',  'Footer text after the page link list');
-	define('_SB_PGPREFIX',  'Prefix text before each page link');
-	define('_SB_PGSUFFIX',  'Suffix text after each page link');
-	define('_SB_PAGEDOTS',  'Text for pagination dots');
-	define('_SB_CURPGPRF',  'Prefix text before current page number');
-	define('_SB_CURPGSUF',  'Suffix text after current page number');
 	define('_TAG_SELECT',   'all blogs|0|currentblog only|1|narrowed with catid/subcatid|2');
 	define('_STICKSELECT',  'show all stickyID|0|show current blog stickyID only|1');
 ?>
--- showblogs/japanese-utf8.php
+++ showblogs/japanese-utf8.php
@@ -14,17 +14,6 @@
 	define('_TAG_MODE',     'NP_TagEX 使用時のページスイッチのモード');
 	define('_SB_NEXTL',     '次のページへのリンクテキスト');
 	define('_SB_PREVL',     '前のページへのリンクテキスト');
-	define('_SB_NEXTSEP',   '次のページへのリンクテキストの前に表示するセパレータ');
-	define('_SB_PREVSEP',   '前のページへのリンクテキストの後に表示するセパレータ');
-	define('_SB_PAGESEP2',  'タイプ2のページスイッチのページ同士の間に表示するセパレータ');
-	define('_SB_PAGESEP3',  'タイプ3のページスイッチのページ同士の間に表示するセパレータ');
-	define('_SB_PGLSTHDR',  'ページへのリンクのリストの前に表示するヘッダ');
-	define('_SB_PGLSTFTR',  'ページへのリンクのリストの後に表示するフッタ');
-	define('_SB_PGPREFIX',  '各ページへのリンクの前に付加するプリフィックス');
-	define('_SB_PGSUFFIX',  '各ページへのリンクの後に付加するサフィックス');
-	define('_SB_PAGEDOTS',  'ページスイッチでページを省略表示するテキスト');
-	define('_SB_CURPGPRF',  '現在表示中のページ番号の前に付加するプリフィックス');
-	define('_SB_CURPGSUF',  '現在表示中のページ番号の後に付加するサフィックス');		
 	define('_TAG_SELECT',   '全ブログの tag を表示|0|'
 						  . '表示中のブログに属する tag のみ表示|1|'
 						  . '表示中のカテゴリ・サブカテゴリに属する tag のみ表示|2');
